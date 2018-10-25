3db084117d18a855348f65e339518cf6c5f944f2
leeing <chunshu.ln@raycloud.com>
2018-10-25 22:20:33 +0800

    更改目录结构

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import BasicExample from './router/router';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+    <BasicExample />
+), document.getElementById('root'));
 registerServiceWorker();

0d214b6e0dca4ac27428c2a1c82b91f5fa6e8131
leeing <chunshu.ln@raycloud.com>
2018-10-26 20:45:05 +0800

    开始实践redux

--- src/store.js
+++ src/store.js
@@ -1,1 +1,12 @@
 import { createStore } from 'redux';
+
+// this is a reducer
+function counter(state, action) {
+    const reducer = {
+        'increament': () => state.count + 1,
+        'decreament': () => state.count - 1
+    };
+    return reducer[action.type];
+}
+
+const store = createStore(counter);

c09138b3c257c4ce4c82dd4443b239379d4ade3f
leeing <chunshu.ln@raycloud.com>
2018-10-29 18:27:03 +0800

    redux优化之reselector

--- src/containers/VisibleTodoList.js
+++ src/containers/VisibleTodoList.js
@@ -1,22 +1,10 @@
 import { connect } from 'react-redux';
-import { toggleTodo, VisibilityFilters } from '../actions';
+import { toggleTodo } from '../actions';
 import TodoList from '../components/TodoList';
-
-const getVisibleTodos = (todos, filter) => {
-    switch(filter) {
-        case VisibilityFilters.SHOW_ALL:
-            return todos;
-        case VisibilityFilters.SHOW_COMPLETED:
-            return todos.filter(t => t.completed);
-        case VisibilityFilters.SHOW_ACTIVE:
-            return todos.filter(t => !t.completed);
-        default:
-            throw new Error(`Unknow filter ${filter}`);
-    }
-}
+import { getVisibleTodos } from '../selector';
 
 const mapStateToProps = state => ({
-    todos: getVisibleTodos(state.todos, state.visibilityFilter)
+    todos: getVisibleTodos(state)
 });
 
 const mapDispatchToProps = dispatch => ({

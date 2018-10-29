8aa25cd67ae46e5fce8270cab6c81121dba082e1
leeing <chunshu.ln@raycloud.com>
2018-10-29 16:34:03 +0800

    删除debugger

--- src/containers/FilterLink.js
+++ src/containers/FilterLink.js
@@ -2,12 +2,9 @@ import { connect } from 'react-redux';
 import { setVisibilityFilter } from '../actions';
 import Link from '../components/Link';
 
-const mapStateToProps = (state, ownProps) => {
-    debugger;
-    return {
-        active: ownProps.filter === state.visibilityFilter
-    }
-};
+const mapStateToProps = (state, ownProps) => ({
+    active: ownProps.filter === state.visibilityFilter
+});
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
     onClick: () => dispatch(setVisibilityFilter(ownProps.filter))
